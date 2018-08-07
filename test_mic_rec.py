8b29df56d26d8c3bde65e2c8cd92d59cb4de345f
Artem Molchanov <a.molchanov86@gmail.com>
2018-08-06 23:56:42 -0700

    test mic rec: spectragram example added

--- test_mic_rec.py
+++ test_mic_rec.py
@@ -4,6 +4,9 @@
 # sudo apt-get install libasound2-dev
 # pip install pyalsaaudio
 
+## Docs on libalsa
+# https://larsimmisch.github.io/pyalsaaudio/libalsaaudio.html
+
 ## This is an example of a simple sound capture script.
 ##
 ## The script opens an ALSA pcm for sound capture. Set
@@ -13,12 +16,19 @@
 ## To test it out, run it and shout at your microphone:
 
 import alsaaudio, time, audioop
+import numpy as np
+import matplotlib.pyplot as plt
 # import matplotlib
 
 # Open the device in nonblocking capture mode. The last argument could
 # just as well have been zero for blocking mode. Then we could have
 # left out the sleep call in the bottom of the loop
-inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
+
+## Nonblockin mode
+# inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
+
+# Normal blocking mode
+inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
 
 # Set attributes: Mono, 8000 Hz, 16 bit little endian samples
 inp.setchannels(1)
@@ -30,17 +40,33 @@ inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 # For our purposes, it is suficcient to know that reads from the device
 # will return this many frames. Each frame being 2 bytes long.
 # This means that the reads below will return either 320 bytes of data
-# or 0 bytes of data. The latter is possible because we are in nonblocking
+# or 0 bytes of data. The latter is possible if we are in nonblocking
 # mode.
 inp.setperiodsize(160)
 
+max_buff_size = 8000
+buffer = np.array([])
+
 # import Queue
 # data_q = Queue.Queue()
+plt.figure(1)
+plt.show(block=False)
+
 
 while True:
     # Read data from device
     l,data = inp.read()
-    # print(data)
+    decoded_block = np.frombuffer(data, dtype='i2')
+    buffer = np.append(buffer, decoded_block)
+    # buffer = buffer.append(decoded_block)
+    buffer = buffer[-max_buff_size:]
+    if buffer.size == max_buff_size:
+        plt.specgram(buffer, NFFT=256, Fs=8000)
+        plt.pause(0.001)
+        plt.draw()
+        buffer = np.array([])
+
+    # print(decoded_block, 'dtype:', type(decoded_block))
     if l:
         # Return the maximum of the absolute value of all samples in a fragment.
         print(audioop.max(data, 2))
